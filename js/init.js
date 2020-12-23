f63cfe8cf60aab7da10ef713f16901df7f3c1591
filipedosreissantos <76508467+filipedosreissantos@users.noreply.github.com>
2020-12-23 02:41:13 -0300

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space 
+
+
+
+alert("Portfólio em migração para o endereço: https://filipefdrs.github.io");
+
+
+
